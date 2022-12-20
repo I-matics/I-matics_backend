790ead6beef275307c32ef28f26c56bc604d1e59
I-matics <118812373+I-matics@users.noreply.github.com>
2022-12-20 13:52:49 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#k=c*&+9g4t^fdwb=er(&%3mgvh#j+afv0d##jw*bfok#15wqb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['i-matics.azurewebsites.net']
 
 
 # Application definition
