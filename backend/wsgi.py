6dc487cc5f1aa040e753be32fd8376de274330c1
I-matics <118812373+I-matics@users.noreply.github.com>
2023-01-02 17:04:58 +0530

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+# from whitenoise import WhiteNoise
+
+# from my_project import MyWSGIApp
+
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root="/path/to/static/files")
+# application.add_files("/path/to/more/static/files", prefix="more-files/")
