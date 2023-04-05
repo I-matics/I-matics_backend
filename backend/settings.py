3c59bf5ff1213408a2a9403652a0c9a608922aca
I-matics <118812373+I-matics@users.noreply.github.com>
2023-04-05 18:19:02 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         'USER': os.environ['DBUSER'],
         'PASSWORD': os.environ['DBPASS'],
         'PORT': '3306',
+        'SSL_VERSION' : ssl.PROTOCOL_TLSv1_2,
         'OPTIONS': {
             'ssl': {'ca': os.getcwd()+'//BaltimoreCyberTrustRoot.crt (1).pem'}
         }
