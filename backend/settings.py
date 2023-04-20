861818b1fc769bcac91fe355d197bbf0cd29efde
I-matics <118812373+I-matics@users.noreply.github.com>
2023-04-20 17:56:25 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -76,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-hostname = os.environ['DBHOST']
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -86,13 +85,10 @@ DATABASES = {
 #         'NAME': ''
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".mysql.database.azure.com",
+        'HOST': os.environ['DBHOST'],
         'USER': os.environ['DBUSER'],
         'PASSWORD': os.environ['DBPASS'],
         'PORT': '3306',
-        'OPTIONS': {
-            'ssl': {'ca': os.getcwd()+'//BaltimoreCyberTrustRoot.crt (1).pem', 'ssl_version': ssl.PROTOCOL_TLSv1_2}
-        }
     }
 }
 
