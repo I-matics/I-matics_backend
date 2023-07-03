299110c8c6c3efd2abb4b902157961e88ee01f92
I-matics <118812373+I-matics@users.noreply.github.com>
2023-07-03 12:56:15 +0530

    Update urls.py
    Added upload-csv url

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("cardetail/", views.hello_world),
     path("random/<int:pk>/", views.generate_random),
     path("trip/<int:id_n>/", views.trip_data),
+    path('upload-csv/', views.upload_csv_api, name='upload_csv_api'),
 ]
