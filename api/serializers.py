3e5da6e43fb9e4f7e880982bed93de719cdb3bae
I-matics <118812373+I-matics@users.noreply.github.com>
2023-07-05 11:04:29 +0530

    Update serializers.py
    Add TripDetail and TripDetailView Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CarDetail, UserDetail
+from .models import CarDetail, UserDetail,TripDetail
 
 
 class CarDetailSerializer(serializers.ModelSerializer):
@@ -14,3 +14,18 @@ class UserdetailSerializer(serializers.ModelSerializer):
         model = UserDetail
         # fields = ['time', 'ax', 'ay', 'az', 'speed', 'trip', 'geoloacation']
         fields = ['id','Trip']
+
+class TripDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TripDetail
+        # fields = ['time', 'ax', 'ay', 'az', 'speed', 'trip', 'geoloacation']
+        # fields = ['Trip_No', 'Risk_Instance', 'Average_speed', 'Trip_time', 'Distance_Travelled', 'Score']
+        fields = '__all__'
+
+class TripDetailsViewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TripDetail
+        # fields = ['time', 'ax', 'ay', 'az', 'speed', 'trip', 'geoloacation']
+        fields = ['Trip_No', 'Risk_Instance', 'Average_speed', 'Trip_time', 'Distance_Travelled', 'Score']
+        # fields = '__all__'
+
