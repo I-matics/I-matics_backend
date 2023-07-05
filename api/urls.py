a6824e5b414d9f02fd0f63a2d6411d9e6a011017
I-matics <118812373+I-matics@users.noreply.github.com>
2023-07-05 11:07:01 +0530

    Update urls.py
    Add Tripdetailcheck url

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("cardetail/", views.hello_world),
     path("random/<int:pk>/", views.generate_random),
     path("trip/<int:id_n>/", views.trip_data),
+    path("trip_detailcheck/<int:id_t>/", views.get_trip_details),
     path('upload-csv/', views.upload_csv_api, name='upload_csv_api'),
 ]
