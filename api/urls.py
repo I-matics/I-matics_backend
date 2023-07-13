45657561ddc4ecbd47958ca0845d829a503974fc
I-matics <118812373+I-matics@users.noreply.github.com>
2023-07-13 17:46:58 +0530

    Update urls.py
    Add mobile number url

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("trip/<int:id_n>/", views.trip_data),
     path("trip_detailcheck/<int:id_t>/", views.get_trip_details),
     path('upload-csv/', views.upload_csv_api, name='upload_csv_api'),
+    path('store-mobile-number/', views.store_mobile_number, name='store-mobile-number'),
 ]
